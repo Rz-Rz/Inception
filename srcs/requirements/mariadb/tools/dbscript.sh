6f7e4e071349edc8085c079b3d75a1901633e7be
Rz-Rz <karimdhrif4444@gmail.com>
2023-10-23 14:32:39 +0200

    s

--- srcs/requirements/mariadb/tools/dbscript.sh
+++ srcs/requirements/mariadb/tools/dbscript.sh
@@ -2,23 +2,27 @@
 
 service mysql start;
 
-# Wait for the database service to start up
-echo "Waiting for MariaDB to start up..."
-sleep 10  # Wait for 10 seconds (or more if needed)
+# Check if the database already exists
+DB_CHECK=`mysql -uroot -p${DB_ROOT_PASS} -e "SHOW DATABASES LIKE '${DB_NAME}'" | grep ${DB_NAME}`
 
-# log into MariaDB as root and create database and the user
-# Since the root user doesn't have a password yet, we don't need to provide one
-mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
-mysql -uroot -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -uroot -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
-mysql -uroot -e "FLUSH PRIVILEGES;"
-mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"  # Now set the root password
-
-mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
-
-# Keep the script running to prevent the container from stopping
-exec mysqld_safe
-
-#print status
-echo "MariaDB database and user were created successfully!"
+if [ "$DB_CHECK" == "${DB_NAME}" ]; then
+	exec mysqld_safe
+	echo "MariaDB database launched"
+else
+	# Wait for the database service to start up
+	echo "Waiting for MariaDB to start up..."
+	sleep 10  # Wait for 10 seconds (or more if needed)
+	# log into MariaDB as root and create database and the user
+	# Since the root user doesn't have a password yet, we don't need to provide one
+	mysql -uroot -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+	mysql -uroot -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+	mysql -uroot -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+	mysql -uroot -e "FLUSH PRIVILEGES;"
+	mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"  # Now set the root password
+	mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+	# Keep the script running to prevent the container from stopping
+	exec mysqld_safe
+	#print status
+	echo "MariaDB database and user were created successfully!"
+fi
 
--- srcs/requirements/wordpress/tools/wpscript.sh
+++ srcs/requirements/wordpress/tools/wpscript.sh
@@ -4,25 +4,25 @@
 cd /var/www/html/wordpress
 
 if ! wp core is-installed; then
-wp config create	--allow-root --dbname=${SQL_DATABASE} \
-			--dbuser=${SQL_USER} \
-			--dbpass=${SQL_PASSWORD} \
-			--dbhost=${SQL_HOST} \
-			--url=https://${DOMAIN_NAME};
+	wp config create	--allow-root --dbname=${SQL_DATABASE} \
+		--dbuser=${SQL_USER} \
+		--dbpass=${SQL_PASSWORD} \
+		--dbhost=${SQL_HOST} \
+		--url=https://${DOMAIN_NAME};
 
-wp core install	--allow-root \
-			--url=https://${DOMAIN_NAME} \
-			--title=${SITE_TITLE} \
-			--admin_user=${ADMIN_USER} \
-			--admin_password=${ADMIN_PASSWORD} \
-			--admin_email=${ADMIN_EMAIL};
+	wp core install	--allow-root \
+		--url=https://${DOMAIN_NAME} \
+		--title=${SITE_TITLE} \
+		--admin_user=${ADMIN_USER} \
+		--admin_password=${ADMIN_PASSWORD} \
+		--admin_email=${ADMIN_EMAIL};
 
-wp user create		--allow-root \
-			${USER1_LOGIN} ${USER1_MAIL} \
-			--role=author \
-			--user_pass=${USER1_PASS} ;
+	wp user create		--allow-root \
+		${USER1_LOGIN} ${USER1_MAIL} \
+		--role=author \
+		--user_pass=${USER1_PASS} ;
 
-wp cache flush --allow-root
+	wp cache flush --allow-root
 
 # it provides an easy-to-use interface for creating custom contact forms and managing submissions, as well as supporting various anti-spam techniques
 wp plugin install contact-form-7 --activate
